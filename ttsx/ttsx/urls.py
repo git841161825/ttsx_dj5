a6bfa4e6671e69358435b010bc5890899432843b
gti841161825 <m17682307732@163.com>
2018-01-12 17:44:10 +0800

    123

--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -18,4 +18,9 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+<<<<<<< Updated upstream
+=======
+    # url(r'^', include('booktest.urls')),
+    # url(r'^user/', include('df_user.urls')),
+>>>>>>> Stashed changes
 ]
