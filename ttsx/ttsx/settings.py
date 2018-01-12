962488e55866b15dbfb36763ba648f046fdc3c68
gti841161825 <m17682307732@163.com>
2018-01-12 17:28:52 +0800

    修改语言和时区

--- ttsx/ttsx/settings.py
+++ ttsx/ttsx/settings.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 Django settings for ttsx project.
 
@@ -85,9 +86,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'  # 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'  # 'UTC'
 
 USE_I18N = True
 
